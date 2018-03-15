998352ebdb39c68c5e8d66019081d2a7c7aadf4a
Diego Custodio <36503491+Diego-Krauss@users.noreply.github.com>
2018-03-15 08:43:05 -0300

    Add files via upload

---
+++ String_Exercicio_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define max 15
+
+int main() {
+int v1[max];
+int i, maior = NULL, menor = NULL;
+	for (i=0;i<max;i++){
+		printf("Digite um número :");
+		scanf("%d",&v1[i]);
+	if (i == 0){
+		maior = v1[i];
+		menor = maior;
+    }else{
+		if (v1[i] > maior){
+			maior = v1[i];
+		}else if (v1[i] < menor){
+	menor = v1[i];
+		}
+	}
+}
+	printf("maior numero = %d, menor numero = %d\n",maior,menor);
+	system("pause");
+	return 0;
+}
+

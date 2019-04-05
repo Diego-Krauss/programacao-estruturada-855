98655485688697300da4d9a7f22daefdab0a7b07
Diego Custodio <36503491+diego-krauss@users.noreply.github.com>
2019-04-04 22:08:29 -0300

    Update and rename string_exercicio_1.c to num-primo.c

--- num-primo.c
+++ num-primo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define max 10
+
 bool numeroPrimo(int numero){
 bool ehPrimo = false;
 	if (numero <= 1 || (numero != 2 && numero % 2 == 0))
@@ -11,10 +12,11 @@ int d = 3;
 	while (ehPrimo && d <= numero / 2) {
 		if (numero % d == 0)
 			ehPrimo = false;
-			d = d + 2;/* testamos so' os impares: 3, 5, 7... */
+			d = d + 2; /* testamos so os impares: 3, 5, 7... */
 		}
 	return ehPrimo;
 	}
+
 int main() {
 int v1[max];
 int i;
@@ -22,8 +24,7 @@ int i;
 		printf("Digite um número :");
 		scanf("%d",&v1[i]);
 	if (numeroPrimo(v1[i])){
-		printf("sou numero primo %d estou na posicao %d
-	do vetor \n",v1[i],i);
+		printf("Sou numero primo %d estou na posicao %d do vetor \n",v1[i],i);
 	};
 }
 	system("pause");
